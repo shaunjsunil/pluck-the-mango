65b53cd8979495cba58b25a4c951bdba32ecdd50
shaunjsunil <73579508+shaunjsunil@users.noreply.github.com>
2021-06-16 22:11:44 +0530

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,34 @@
+class mango{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:true,
+			restitution :0,
+            friction :1,
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("images/mango.png")
+		this.body=Bodies.circle(this.x, this.y, this.r, options)
+		World.add(world, this.body);
+	}
+
+	display()
+	{
+		var mangoPos=this.body.position;
+		
+		push()
+		//var angle =this.body.angle;	
+		translate(mangoPos.x, mangoPos.y);
+		// rectMode(CENTER);
+	//	rotate(angle)
+		//translate(an)
+		fill(255,0,255)
+		imageMode(CENTER);
+		ellipseMode(CENTER);
+		image(this.image, 0,0,this.r*2, this.r*2)
+		
+		pop()
+ }
+}
